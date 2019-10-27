db0ab49d35aa0044326fe3a55bdff1ff9a68e015
Emma Stotz <stotz.emma@gmail.com>
2019-10-26 22:26:39 -0400

    Initial file upload. HTML completed. Initial styling has begun.

---
+++ app/data/friend.js
@@ -0,0 +1,99 @@
+var friendOptions = [{
+  "name": "SpongeBob SquarePants",
+  "photo": "..public/images/spongebob.png",
+  "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+  ]
+},
+{
+  "name": "Patrick Star",
+  "photo": "..public/images/patrickstar.png",
+  "scores": [
+      1,
+      5,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+  ]
+},
+{
+  "name": "Squidward Tentacles",
+  "photo": "..public/images/squidward.png",
+  "scores": [
+      2,
+      3,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2
+  ]
+},
+{
+  "name": "Plankton",
+  "photo": "..public/images/plankton.png",
+  "scores": [
+      3,
+      3,
+      1,
+      5,
+      2,
+      3,
+      2,
+      2,
+      4,
+      5
+  ]
+},
+{
+  "name": "Mr. Krabs",
+  "photo": "..public/images/mrkrabs.png",
+  "scores": [
+      3,
+      3,
+      3,
+      4,
+      1,
+      1,
+      3,
+      1,
+      1,
+      3
+  ]
+},
+{
+  "name": "Plankton",
+  "photo": "..public/images/sandycheeks.png",
+  "scores": [
+      4,
+      3,
+      2,
+      1,
+      2,
+      5,
+      1,
+      1,
+      5,
+      5
+  ]
+}
+];
+
+module.exports = friendOptions;
