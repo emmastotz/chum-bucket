a1ad3ec33d5db8f5b2d9579481f85974afc4b8cd
Emma Stotz <stotz.emma@gmail.com>
2019-10-29 15:35:15 -0400

    Modal is working (somehow). Image still not displaying.

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,6 +1,6 @@
 var friendOptions = [{
   "name": "SpongeBob SquarePants",
-  "photo": "./public/assets/.images/spongebob.png",
+  "photo": "C:/Users/stotz/github_repo/Homework/chum-bucket/app/public/assets/images/spongebob.png",
   "scores": [
       5,
       1,
@@ -16,7 +16,7 @@ var friendOptions = [{
 },
 {
   "name": "Patrick Star",
-  "photo": "../public/assets/images/patrickstar.png",
+  "photo": "https://en.wikipedia.org/wiki/File:Patrick_Star.svg",
   "scores": [
       1,
       5,
@@ -32,7 +32,7 @@ var friendOptions = [{
 },
 {
   "name": "Squidward Tentacles",
-  "photo": "../public/assets/images/squidward.png",
+  "photo": "C:/Users/stotz/github_repo/Homework/chum-bucket/app/public/assets/images/squidward.png",
   "scores": [
       2,
       3,
@@ -48,7 +48,7 @@ var friendOptions = [{
 },
 {
   "name": "Plankton",
-  "photo": "../public/assets/images/plankton.png",
+  "photo": "C:/Users/stotz/github_repo/Homework/chum-bucket/app/public/assets/images/plankton.png",
   "scores": [
       3,
       3,
@@ -64,7 +64,7 @@ var friendOptions = [{
 },
 {
   "name": "Mr. Krabs",
-  "photo": "../public/assets/images/mrkrabs.png",
+  "photo": "C:/Users/stotz/github_repo/Homework/chum-bucket/app/public/assets/images/mrkrabs.png",
   "scores": [
       3,
       3,
@@ -80,7 +80,7 @@ var friendOptions = [{
 },
 {
   "name": "Sandy Cheeks",
-  "photo": "../public/assets/images/sandycheeks.png",
+  "photo": "C:/Users/stotz/github_repo/Homework/chum-bucket/app/public/assets/images/sandycheeks.png",
   "scores": [
       4,
       3,
